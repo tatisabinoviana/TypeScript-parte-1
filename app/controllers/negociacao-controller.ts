a4742595f9276fab7a49119f80dc15d5bd1bf49e
Tatiana Viana <tatisabinoviana@gmail.com>
2021-08-10 16:35:52 -0300

    feito o enum dias da semana, criada a função criaDe que é static e pertence a classe Negociacao

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiasDaSemana } from './../enums/dias-da-semana.js';
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from './../models/negociacoes.js';
 import { MensagemView } from './../views/mensagem-view.js';
@@ -10,8 +11,6 @@ export class NegociacaoController {
   private negociacoes = new Negociacoes();
   private negociacoesView = new NegociacoesView('#negociacoesView');
   private mensagemView = new MensagemView('#mensagemView');
-  private DOMINGO = 0;
-  private SABADO = 6;
 
   constructor() {
     this.inputData = document.querySelector('#data');
@@ -21,27 +20,24 @@ export class NegociacaoController {
   }
 
   public adiciona(): void {
-    const negociacao = this.criaNegociacao();
+    const negociacao = Negociacao.criaDe(
+      this.inputData.value,
+      this.inputQuantidade.value,
+      this.inputValor.value
+    );
     if(!this.ehDiaUtil(negociacao.data)) {
       this.mensagemView
         .update('Apenas negociações em dias úteis são aceitas');
       return;
     }
-    
+
     this.negociacoes.adiciona(negociacao);
     this.limparFormulario();
     this.atualizaView();
   }
   private ehDiaUtil(data: Date) {
-    return data.getDay() > this.DOMINGO && data.getDay() < this.SABADO;
-  }
-  
-  private criaNegociacao(): Negociacao{
-    const exp = /-/g;
-    const date = new Date(this.inputData.value.replace(exp, ','));
-    const quantidade = parseInt(this.inputQuantidade.value);
-    const valor = parseFloat(this.inputValor.value);
-    return new Negociacao(date, quantidade, valor);
+    return data.getDay() > DiasDaSemana.DOMINGO
+          && data.getDay() < DiasDaSemana.SABADO;
   }
 
   private limparFormulario(): void {
---
+++ dist/js/enums/dias-da-semana.js
@@ -0,0 +1,10 @@
+export var DiasDaSemana;
+(function (DiasDaSemana) {
+    DiasDaSemana[DiasDaSemana["DOMINGO"] = 0] = "DOMINGO";
+    DiasDaSemana[DiasDaSemana["SEGUNDA"] = 1] = "SEGUNDA";
+    DiasDaSemana[DiasDaSemana["TERCA"] = 2] = "TERCA";
+    DiasDaSemana[DiasDaSemana["QUANTA"] = 3] = "QUANTA";
+    DiasDaSemana[DiasDaSemana["QUINTA"] = 4] = "QUINTA";
+    DiasDaSemana[DiasDaSemana["SEXTA"] = 5] = "SEXTA";
+    DiasDaSemana[DiasDaSemana["SABADO"] = 6] = "SABADO";
+})(DiasDaSemana || (DiasDaSemana = {}));
